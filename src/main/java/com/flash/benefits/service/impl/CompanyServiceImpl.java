204522230722ee63fee4c0f307a15789ccebe91a
Silvia-Mart <santosmartinssilvia@gmail.com>
2023-06-14 19:50:46 -0300

    Adding the CompanyService interface to the implementation abstraction

--- src/main/java/com/flash/benefits/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/flash/benefits/service/impl/CompanyServiceImpl.java
@@ -1,16 +1,16 @@
-package com.flash.benefits.service;
+package com.flash.benefits.service.impl;
 
 import com.flash.benefits.model.CompanyModel;
 import com.flash.benefits.repository.CompanyRepository;
+import com.flash.benefits.service.CompanyService;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
-public class CompanyService {
-
+public class CompanyServiceImpl implements CompanyService {
     CompanyRepository companyRepository;
-    CompanyService(CompanyRepository companyRepository){
+    CompanyServiceImpl(CompanyRepository companyRepository){
         this.companyRepository = companyRepository;
     }
     @Transactional
