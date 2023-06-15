14ac25d5910b1003778e10715e9f6b322bc06f1c
Silvia-Mart <santosmartinssilvia@gmail.com>
2023-06-15 15:31:59 -0300

    Adding a method to search by company registration number before registering

--- src/main/java/com/flash/benefits/repository/CompanyRepository.java
+++ src/main/java/com/flash/benefits/repository/CompanyRepository.java
@@ -4,4 +4,5 @@ import com.flash.benefits.model.CompanyModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<CompanyModel, Long> {
+    public Boolean existsByCnpj(String cnpj);
 }
