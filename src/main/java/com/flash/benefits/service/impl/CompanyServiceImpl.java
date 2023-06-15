9e089f94041fb5ac1a1b5c95b81936f903eefe70
Silvia-Mart <santosmartinssilvia@gmail.com>
2023-06-15 15:33:07 -0300

    Reformulating the business rule for company registration through cnpj validation

--- src/main/java/com/flash/benefits/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/flash/benefits/service/impl/CompanyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.flash.benefits.service.impl;
 
+import com.flash.benefits.exception.EntityException;
 import com.flash.benefits.model.CompanyModel;
 import com.flash.benefits.repository.CompanyRepository;
 import com.flash.benefits.service.CompanyService;
@@ -15,6 +16,10 @@ public class CompanyServiceImpl implements CompanyService {
     }
     @Transactional
     public CompanyModel registerCompany(CompanyModel companyModel){
-        return companyRepository.save(companyModel);
+        if(companyRepository.existsByCnpj(companyModel.getCnpj()) == false){
+            return companyRepository.save(companyModel);
+        }
+        throw new EntityException("It is not possible to register the company because this cnpj is already being used in the system.");
+
     }
 }
