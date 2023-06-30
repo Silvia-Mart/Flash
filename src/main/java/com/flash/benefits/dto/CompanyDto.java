6ee694e713d2c357cf95d207ce0848316370b6be
Silvia-Mart <santosmartinssilvia@gmail.com>
2023-06-30 18:07:03 -0300

    Adding more attribute fields for the model and dto and changing the id to the UUID type.

--- src/main/java/com/flash/benefits/dto/CompanyDto.java
+++ src/main/java/com/flash/benefits/dto/CompanyDto.java
@@ -4,6 +4,7 @@ import com.flash.benefits.model.CompanyModel;
 import org.hibernate.validator.constraints.br.CNPJ;
 
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public class CompanyDto {
@@ -12,12 +13,25 @@ public class CompanyDto {
     @NotBlank
     @CNPJ
     private String cnpj;
+    @NotBlank
+    private String companyPhone;
+    @NotBlank
+    @Email
+    private String emailCompany;
+    @NotBlank
+    private String namePresident;
+    @NotBlank
+    private String presidentPhone;
+    @NotBlank
+    @Email
+    private String emailPresident;
 
     public CompanyDto() {
     }
 
     public CompanyModel transferDataCompanyModel(){
-        return new CompanyModel(name,cnpj);
+        return new CompanyModel(name,cnpj,companyPhone,
+                emailCompany,namePresident,presidentPhone,emailPresident);
     }
 
     public String getName() {
@@ -36,11 +50,56 @@ public class CompanyDto {
         this.cnpj = cnpj;
     }
 
+    public String getCompanyPhone() {
+        return companyPhone;
+    }
+
+    public void setCompanyPhone(String companyPhone) {
+        this.companyPhone = companyPhone;
+    }
+
+    public String getEmailCompany() {
+        return emailCompany;
+    }
+
+    public void setEmailCompany(String emailCompany) {
+        this.emailCompany = emailCompany;
+    }
+
+    public String getNamePresident() {
+        return namePresident;
+    }
+
+    public void setNamePresident(String namePresident) {
+        this.namePresident = namePresident.toUpperCase();
+    }
+
+    public String getPresidentPhone() {
+        return presidentPhone;
+    }
+
+    public void setPresidentPhone(String presidentPhone) {
+        this.presidentPhone = presidentPhone;
+    }
+
+    public String getEmailPresident() {
+        return emailPresident;
+    }
+
+    public void setEmailPresident(String emailPresident) {
+        this.emailPresident = emailPresident;
+    }
+
     @Override
     public String toString() {
         return "CompanyDto{" +
                 "name='" + name + '\'' +
                 ", cnpj='" + cnpj + '\'' +
+                ", companyPhone='" + companyPhone + '\'' +
+                ", emailCompany='" + emailCompany + '\'' +
+                ", namePresident='" + namePresident + '\'' +
+                ", presidentPhone='" + presidentPhone + '\'' +
+                ", emailPresident='" + emailPresident + '\'' +
                 '}';
     }
 }
--- src/main/java/com/flash/benefits/model/CompanyModel.java
+++ src/main/java/com/flash/benefits/model/CompanyModel.java
@@ -1,33 +1,46 @@
 package com.flash.benefits.model;
 
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 public class CompanyModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long idCompany;
+    String uuidCompany = UUID.randomUUID().toString();
     private String name;
     private String cnpj;
-
     private LocalDateTime dateOfRegistration = LocalDateTime.now();
+    private String companyPhone;
+    private String emailCompany;
+    private String namePresident;
+    private String presidentPhone;
+    private String emailPresident;
 
     public CompanyModel() {
     }
 
-    public CompanyModel(String name, String cnpj) {
+    public CompanyModel(String name, String cnpj, String companyPhone, String emailCompany,
+                        String namePresident, String presidentPhone, String emailPresident) {
+
         this.name = name;
         this.cnpj = cnpj;
+        this.companyPhone = companyPhone;
+        this.emailCompany = emailCompany;
+        this.namePresident = namePresident;
+        this.presidentPhone = presidentPhone;
+        this.emailPresident = emailPresident;
+
     }
 
-    public Long getIdCompany() {
-        return idCompany;
+    public String getUuidCompany() {
+        return uuidCompany;
     }
 
-    public void setIdCompany(Long idCompany) {
-        this.idCompany = idCompany;
+    public void setUuidCompany(String uuidCompany) {
+        this.uuidCompany = uuidCompany;
     }
 
     public String getName() {
@@ -54,13 +67,58 @@ public class CompanyModel {
         this.dateOfRegistration = dateOfRegistration;
     }
 
+    public String getCompanyPhone() {
+        return companyPhone;
+    }
+
+    public void setCompanyPhone(String companyPhone) {
+        this.companyPhone = companyPhone;
+    }
+
+    public String getEmailCompany() {
+        return emailCompany;
+    }
+
+    public void setEmailCompany(String emailCompany) {
+        this.emailCompany = emailCompany;
+    }
+
+    public String getNamePresident() {
+        return namePresident;
+    }
+
+    public void setNamePresident(String namePresident) {
+        this.namePresident = namePresident;
+    }
+
+    public String getPresidentPhone() {
+        return presidentPhone;
+    }
+
+    public void setPresidentPhone(String presidentPhone) {
+        this.presidentPhone = presidentPhone;
+    }
+
+    public String getEmailPresident() {
+        return emailPresident;
+    }
+
+    public void setEmailPresident(String emailPresident) {
+        this.emailPresident = emailPresident;
+    }
+
     @Override
     public String toString() {
         return "CompanyModel{" +
-                "idCompany=" + idCompany +
+                "uuidCompany='" + uuidCompany + '\'' +
                 ", name='" + name + '\'' +
                 ", cnpj='" + cnpj + '\'' +
                 ", dateOfRegistration=" + dateOfRegistration +
+                ", companyPhone='" + companyPhone + '\'' +
+                ", emailCompany='" + emailCompany + '\'' +
+                ", namePresident='" + namePresident + '\'' +
+                ", presidentPhone='" + presidentPhone + '\'' +
+                ", emailPresident='" + emailPresident + '\'' +
                 '}';
     }
 }
